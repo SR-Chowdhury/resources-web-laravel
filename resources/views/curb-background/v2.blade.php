c841a4122048b19a49967960944108854e0f1864
SR-Chowdhury <hackedgdown@gmail.com>
2021-07-03 23:22:59 +0600

    Curv-background v3 done

--- resources/views/curb-background/v2.blade.php
+++ resources/views/curb-background/v2.blade.php
@@ -15,8 +15,6 @@
     <style>
         body {
             font-family: 'Nunito', sans-serif;
-            margin: 0;
-            padding: 0;
         }
     </style>
   </head>
@@ -24,7 +22,7 @@
 
     <section class="curved-background">
         <div class="curved">
-            <h1>Curved v2</h1>
+            <h1 class="text-center py-5">Curved v2 | getwaves.io</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at? Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ducimus nobis eaque eum, blanditiis culpa aut dolore eius harum alias?</p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#fff" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
         </div>
@@ -32,7 +30,7 @@
 
     <section>
         <div class="container">
-            <h1 class="">hello</h1>
+            <h1 class="py-5">hello</h1>
         </div>
     </section>
 
